ffad7fa34beebb9c8e74c4011520372f77abe025
Faisal Samsuri <42949355+faisalsyamsuri13@users.noreply.github.com>
2022-04-20 11:10:06 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>Supreme v0.1 Alpha</title>
+  <link rel="stylesheet" href="assets/css/indexstyle.css">
+</head>
+
+<body align="center">
+  <div id="main">
+    <img src="https://upload.wikimedia.org/wikipedia/en/a/ae/President_University_Logo.png" alt="pu_logo">
+    <h1 align="center">Welcome to Supreme v0.1 Alpha</h1>
+    <hr>
+    <h4 align="center"><a class="one" href="datarecap.php">Data Recapitulation</a></h4>
+    <h4 align="center"><a class="one" href="identification.html">Identification (Using QR Scanner)</a></h4>
+    <hr>
+  </div>
+  <div id="footer">
+    <p class="one" align="center">
+      &copy; 2022 President University. All rights reserved. <a href="about.html">About</a> |
+      <a href="contactus.html">Contact Us</a>
+    </p>
+  </div>
+</body>
+</html>
