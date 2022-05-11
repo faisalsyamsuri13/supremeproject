f26434063e3ec23bf43ada5ec43501aee181e85e
Faisal Samsuri <42949355+faisalsyamsuri13@users.noreply.github.com>
2022-05-11 19:21:00 +0700

    Add files via upload

---
+++ raspi_user/exec.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+ECHO (c) Microsoft Corporation. All rights reserved. &ECHO.
+ECHO The command has been executed, the data is being delivered to the server. Please wait. . .
+ECHO =========================================== &ECHO.
+SET ip_address="IPv4 Address"
+FOR /f "usebackq tokens=2 delims=:" %%f in (`ipconfig ^| findstr /c:%ip_address%`) DO ECHO Message from the server%%f: &ECHO.
+py -c "import comm_test; comm_test.response()" &ECHO.
+ECHO ===========================================
+TIMEOUT /t 3
