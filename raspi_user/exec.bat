62851f8109f449e38332cd5da4605473ff551499
Faisal Samsuri <42949355+faisalsyamsuri13@users.noreply.github.com>
2022-05-11 19:48:25 +0700

    Updating exec.bat (adding timeout)

--- raspi_user/exec.bat
+++ raspi_user/exec.bat
@@ -1,9 +1,12 @@
 @ECHO OFF
 ECHO (c) Microsoft Corporation. All rights reserved. &ECHO.
 ECHO The command has been executed, the data is being delivered to the server. Please wait. . .
+TIMEOUT 1 > NUL
 ECHO =========================================== &ECHO.
 SET ip_address="IPv4 Address"
 FOR /f "usebackq tokens=2 delims=:" %%f IN (`ipconfig ^| FINDSTR /c:%ip_address%`) DO ECHO Message from the server%%f: &ECHO.
+TIMEOUT 1 > NUL
 py -c "import comm_test; comm_test.response()" &ECHO.
 ECHO ===========================================
-TIMEOUT /t 3
+ECHO Exiting. . .
+TIMEOUT 3 > NUL
